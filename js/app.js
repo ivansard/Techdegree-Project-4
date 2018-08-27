6f93467df86366587a73a614164bd172d8f7e278
Ivan Sardelić <ivan.ssardelic@gmail.com>
2018-08-27 18:02:47 +0200

    Created app.js file

---
+++ js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () = > {
+
+  const bodyElement = document.querySelector('body');
+  body.innerHtml = `<div class="screen screen-start" id="start">
+                      <header>
+                        <h1>Tic Tac Toe</h1>
+                        <a href="#" class="button">Start game</a>
+                      </header>
+                    </div>`;
+
+
+
+})
